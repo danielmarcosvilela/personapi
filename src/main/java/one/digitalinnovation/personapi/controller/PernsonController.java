96729b98b1f11d18f22f4992ff2271dce43643c6
Daniel <danielmarcosvilela@gmail.com>
2021-06-30 05:26:18 -0300

    Developed person create operation

--- src/main/java/one/digitalinnovation/personapi/controller/PernsonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PernsonController.java
@@ -1,15 +1,30 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.service.PersonServise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PernsonController {
 
-    @GetMapping
-    public String getBook() {
-        return "API Test!";
+    private PersonServise personServise;
+
+    @Autowired
+    public PernsonController(PersonServise personServise) {
+        this.personServise = personServise;
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personServise.createPerson(person);
+
+    }
+
+
 }
