616137c936b5262903cc9c817a8a9eeec510a206
Daniel <danielmarcosvilela@gmail.com>
2021-07-03 03:25:35 -0300

    Create Operation Final

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -1,24 +1,14 @@
 package one.digitalinnovation.personapi.entity;
 
-import java.time.LocalDate;
-import java.util.List;
-
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
+
 
 @Entity
 @Data
